ba51ea932c0de05815441677b2b93e7481762398
Kunall <146503735+Kunals0612@users.noreply.github.com>
2024-05-10 19:16:11 +0530

    Update Assignment_8.cpp

--- Assignment_8.cpp
+++ Assignment_8.cpp
@@ -1,144 +1,106 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
-    int key;
-    Node *left = nullptr, *right = nullptr;
+class OptimalBST {
+private:
+    float a[20], b[20], wt[20][20], c[20][20];
+    int r[20][20], n;
+    int keys[20]; // Array to store keys
 
 public:
-    friend class OBST;
-};
-
-class OBST
-{
-    int R[100][100];
-    int C[100][100];
-    Node *root = nullptr;
-
-    int sumOfFrequencies(int *arr, int low, int high)
-    {
-        int sum = 0;
-        for (int i = low; i < high; i++)
-        {
-            sum = sum + arr[i];
+    void inputProbabilitiesAndKeys() {
+        int i;
+        cout << "\nEnter the no. of nodes : ";
+        cin >> n;
+        cout << "\nEnter the keys:\n";
+        for (i = 1; i <= n; i++) {
+            cout << "Key[" << i << "]: ";
+            cin >> keys[i];
         }
-        return sum;
+        cout << "\nEnter the probability for successful search:\n";
+        for (i = 1; i <= n; i++) {
+            cout << "p[" << keys[i] << "]: ";
+            cin >> a[i];
+        }
+        cout << "\nEnter the probability for unsuccessful search:\n";
+        keys[0]=0;
+        for (i = 1; i <= n; i++) {
+            cout << "q[" << keys[i] << "]: ";
+            cin >> b[i];
+        }
     }
 
-    void findOptimalSubTree(int *freqs, int numNodes, int n)
-    {
-        for (int i = 0; i < n - numNodes; i++)
-        {
-            int j = numNodes + i;
-            int minCost = 9999;
-            int minRoot = R[i][j - 1];
-            for (int k = i + 1; k <= j; k++)
-            {
-                int cost = C[i][k - 1] + C[k][j];
-                if (cost < minCost)
-                {
-                    minCost = cost;
-                    minRoot = k;
+    void constructOBST() {
+        initializeOBST();
+        calculateOBST();
+        printOptimalBST();
+    }
+
+    void showSearchCosts() {
+        cout << "\n\nSearch costs for each key:\n";
+        for (int i = 1; i <= n; ++i) {
+            cout << "Cost of search for key " << keys[i] << ": " << c[i - 1][i] << endl;
+        }
+    }
+
+private:
+    void initializeOBST() {
+        // Initialization of the wt, c, and r arrays for i-i and i-i+1 cases
+        for (int i = 0; i < n; i++) {
+            c[i][i] = 0.0;
+            r[i][i] = 0;
+            wt[i][i] = b[i];
+            wt[i][i + 1] = b[i] + b[i + 1] + a[i + 1];
+            c[i][i + 1] = b[i] + b[i + 1] + a[i + 1];
+            r[i][i + 1] = i + 1;
+        }
+        c[n][n] = 0.0;
+        r[n][n] = 0;
+        wt[n][n] = b[n];
+    }
+
+    void calculateOBST() {
+        // Calculation of the wt, c, and r arrays for j-i=2,3,4,...,n cases
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= n - i; j++) {
+                wt[j][j + i] = b[j + i] + a[j + i] + wt[j][j + i - 1];
+                c[j][j + i] = 9999;
+                for (int l = j + 1; l <= j + i; l++) {
+                    if (c[j][j + i] > (c[j][l - 1] + c[l][j + i])) {
+                        c[j][j + i] = c[j][l - 1] + c[l][j + i];
+                        r[j][j + i] = l;
+                    }
                 }
-            }
-            C[i][j] = minCost + sumOfFrequencies(freqs, i, j);
-            R[i][j] = minRoot;
-        }
-    }
-
-    Node *constructOBST(int i, int j, int *values)
-    {
-        if (i == j)
-        {
-            return nullptr;
-        }
-        else
-        {
-            Node *node = new Node();
-            node->key = values[R[i][j] - 1];
-            node->left = constructOBST(i, R[i][j] - 1, values);
-            node->right = constructOBST(R[i][j], j, values);
-            return node;
-        }
-    }
-
-    void inOrder(Node *node)
-    {
-        if (node == nullptr)
-            return;
-
-        inOrder(node->left);
-        cout << node->key << " ";
-        inOrder(node->right);
-    }
-
-    void preOrder(Node *node)
-    {
-        if (node == nullptr)
-            return;
-
-        cout << node->key << " ";
-        preOrder(node->left);
-        preOrder(node->right);
-    }
-
-public:
-    void display()
-    {
-        inOrder(this->root);
-        cout << endl;
-        preOrder(this->root);
-    }
-
-    void findOBST(int *values, int *freqs, int totalNodes)
-    {
-        int n = totalNodes + 1;
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                C[i][j] = 0;
-                R[i][j] = 0;
+                c[j][j + i] += wt[j][j + i];
             }
         }
+    }
 
-        for (int i = 0; i < n; i++)
-        {
-            int j = i + 1;
-            C[i][j] = freqs[i];
-            R[i][j] = j;
-        }
+    void printOptimalBST() {
+        cout << "\n\nOptimal BST is :: ";
+        cout << "\nw[0][" << n << "] :: " << wt[0][n];
+        cout << "\nc[0][" << n << "] :: " << c[0][n];
+        cout << "\nr[0][" << n << "] :: " << r[0][n];
+        // printTreeStructure(0, n);
+    }
 
-        for (int i = 2; i < n; i++)
-        {
-            findOptimalSubTree(freqs, i, n);
-        }
-
-        cout << "Cost of OBST is " << C[0][n - 1] << endl;
-
-        this->root = new Node();
-        this->root = constructOBST(0, n - 1, values);
+    void printTreeStructure(int l1, int r1) {
+        if (l1 >= r1)
+            return;
+        if (r[l1][r[l1][r1] - 1] != 0)
+            cout << "\n Left child of " << keys[r[l1][r1] - 1] << " :: " << keys[r[l1][r[l1][r1] - 1]];
+        if (r[r[l1][r1]][r1] != 0)
+            cout << "\n Right child of " << keys[r[l1][r1] - 1] << " :: " << keys[r[r[l1][r1]][r1] - 1];
+        printTreeStructure(l1, r[l1][r1] - 1);
+        printTreeStructure(r[l1][r1], r1);
     }
 };
 
-int main()
-{
-    OBST t;
-    int nodes;
-    cout << "Enter no of nodes: ";
-    cin >> nodes;
-    int values[nodes];
-    int frequencies[nodes];
-    for (int i = 0; i < nodes; i++)
-    {
-        cout << "Enter value and frequency: ";
-        cin >> values[i] >> frequencies[i];
-    }
-    cout << endl;
-    t.findOBST(values, frequencies, nodes);
-    t.display();
-
+int main() {
+    OptimalBST obst;
+    cout << "\n**** PROGRAM FOR OBST ******\n";
+    obst.inputProbabilitiesAndKeys();
+    obst.constructOBST();
+    obst.showSearchCosts();
     return 0;
 }
